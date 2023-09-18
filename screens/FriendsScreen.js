b71eadbecd5b069341bc5681a91b109ce5833a31
shivam sheokand <shivamsheokand32@gmail.com>
2023-09-18 09:32:59 +0530

    fix some errors

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -1,75 +1,12 @@
-// import { StyleSheet, Text, View } from "react-native";
-// import React, { useEffect, useContext, useState } from "react";
-// import axios from "axios";
-// import { UserType } from "../UserContext";
-// // import FriendRequest from "../components/FriendRequest";
-
-// const FriendsScreen = () => {
-//     const { userid, setUserid } = useContext(UserType);
-//     const [friendRequests, setFriendRequests] = useState([]);
-//     useEffect(() => {
-//         fetchFriendRequests();
-//     }, []);
-//     const fetchFriendRequests = async () => {
-//         try {
-//             const response = await axios.get(
-//                 `http://192.168.1.3:8000/friend-request/${userid}`
-//             );
-//             if (response.status === 200) {
-//                 const friendRequestsData = response.data.map((friendRequest) => ({
-//                     _id: friendRequest._id,
-//                     name: friendRequest.name,
-//                     email: friendRequest.email,
-//                 }));
-
-//                 setFriendRequests(friendRequestsData);
-//             }
-//         } catch (err) {
-//             console.log("error message", err);
-//         }
-//     };
-
-//     console.log(friendRequests);
-//     return (
-//         <View style={{ padding: 10, marginHorizontal: 12 }}>
-//             {friendRequests.length > 0 && <Text>Your Friend Requests!</Text>}
-//         </View>
-//     );
-// };
-
-// export default FriendsScreen;
-// const styles = StyleSheet.create({});
-
-    // const fetchFriendRequests = async () => {
-    //     try {
-    //         const response = await axios.get(
-    //             `http://192.168.1.3:8000/friend-request/${userid}`
-    //         );
-    //         if (response.status === 200) {
-    //             const friendRequestsData = response.data.map((friendRequest) => ({
-    //                 _id: friendRequest._id,
-    //                 name: friendRequest.name,
-    //                 email: friendRequest.email,
-    //             }));
-
-    //             setFriendRequests(friendRequestsData);
-    //         }
-    //     } catch (err) {
-    //         console.error("Error message", err); // Use console.error for errors
-    //     }
-    // };
-
-
-
 import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useContext, useState } from "react";
 import axios from "axios";
 import { UserType } from "../UserContext";
+// import FriendRequest from "../components/FriendRequest";
 
 const FriendsScreen = () => {
-    const { userid} = useContext(UserType);
+    const { userid } = useContext(UserType);
     const [friendRequests, setFriendRequests] = useState([]);
-    
     useEffect(() => {
         fetchFriendRequests();
     }, []);
@@ -79,42 +16,37 @@ const FriendsScreen = () => {
             const response = await axios.get(
                 `http://192.168.1.3:8000/friend-request/${userid}`
             );
-            console.log("Server Response:", response.data); // Log the server response
-            // ...
-        } catch (err) {
-            console.error("Error message", err);
-        }
-    };
-    
-    const fetchFriendRequests1 = async () => {
-        try {
-            const response = await axios.get(
-                `http://192.168.1.3:8000/friend-request/${userid}`
-            );
-            if (response.status === 200 && Array.isArray(response.data)) {
+            if (response.status === 200) {
                 const friendRequestsData = response.data.map((friendRequest) => ({
                     _id: friendRequest._id,
                     name: friendRequest.name,
                     email: friendRequest.email,
                 }));
-    
+
                 setFriendRequests(friendRequestsData);
-            } else {
-                console.error("Invalid response data:", response.data);
             }
         } catch (err) {
-            console.error("Error message", err); // Use console.error for errors
+            console.log("error message", err);
         }
     };
-    
+
     console.log(friendRequests);
     return (
         <View style={{ padding: 10, marginHorizontal: 12 }}>
             {friendRequests.length > 0 && <Text>Your Friend Requests!</Text>}
+
+            {friendRequests.map((item, index) => (
+                {/* <FriendRequest
+                    key={index}
+                    item={item}
+                    friendRequests={friendRequests}
+                    setFriendRequests={setFriendRequests}
+                /> */}
+            ))}
         </View>
     );
 };
 
 export default FriendsScreen;
-const styles = StyleSheet.create({});
 
+const styles = StyleSheet.create({});
